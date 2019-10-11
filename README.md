0cfd423c8d92567565a1847fbd38236fe70e6d01
David E Cermeno Pizon <daecepi@gmail.com>
2019-10-11 01:58:43 -0500

    improvemetn ithe cde

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Queen's challenge
+_By: David Eduardo Cermeño Pinzón_
 
-## Installation
+__Note:__ Since the evaluators can be anyone inside the company the documentation was let simple and quite explanatory
+
+# Prerequisites
+- Having python installed
+
+## Installation and usage
+1. Download or clone the repo
+2. Setup the folder in a place you are confortable working in
+3. Put your inputs inside the file: "InputsQueenBoard.txt"
+4. Open a console and go to the path where this project is in
+5. Run the command: "python script.py"
 
 ## Dependencies
+- Since no external libraries were needed only python version 3.6 or older
 
 ## Exercise description
+
+## Approaches
+- Modularizing the code as much as possible according to requerements actual understanding
--- script.py
+++ script.py
@@ -1,19 +1,47 @@
 '''
-Main functionality that contains the application
+Main functionality that contains the application's main funtional requirement
 '''
-def queenCalculator(matt):
-    print(1)
+def queen_calculation(board_size, queen_pos, obstacles):
+    
+    #Get all the obstacle
+    closest_obstacles = get_first_obstacles(obstacles)
+
+'''
+Function that finds the obstacles of the eight possition
+    @Param list(tuples) : 
+'''
+def get_first_obstacles(obstacles):
+    closest_obstacles = []
+    #Get first four obstacles (the cross ones)
 
 
-def getFirstObstacles():
-    print(2)
+    return closest_obstacles
+    
 
-def getFileInfo():
-    finalInfo = []
-    data = []
+'''
+Modularizing the reading of the file since the responsability of reading the file
+does not necesarily belong to the queen calculator.
+    @returns inputs : the inputs organized orginized as a list of tuples
+'''
+def get_file_inputs_for_analisis():
+    fileText = []
     with open('./InputsQueenBoard.txt', 'r') as f:
-        data = f.read()
-    print(type(data))
+        fileText = f.read()
+    
+    #Pythonic way of spliting strings into lists with data
+    #Also the raw inputs will contain the different outputs
+    rawInputs = fileText.split("\n")
 
+    #Treating the raw input to a more managable data structure (tuples given the nature of the data)
+    treatedInputs = [tuple(map(int, inputPair.split(' '))) for inputPair in rawInputs]
+
+    return treatedInputs
+
+#Checking entrypoint to prevent execution if this set of functions are imported as any python module
 if __name__ == "__main__" :
-    getFileInfo()
+
+    #Getting the inputs for the main function
+    inputs = get_file_inputs_for_analisis()
+
+    #Calling the main algorithm
+    queen_calculation(inputs[0], inputs[1], [])
